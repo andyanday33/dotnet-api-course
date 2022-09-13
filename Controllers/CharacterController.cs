b7b150e4aff92a1d027352d4ca88bc25fdf15cc9
berke <bab26@st-andrews.ac.uk>
2022-09-13 19:41:01 +0300

    feat: add new controller to get single character

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -11,14 +11,20 @@ namespace dotnet_api.Controllers
     [Route("api/[controller]")]
     public class CharacterController : ControllerBase
     {
-        private static List<Character> knights = new List<Character> {
+        private static List<Character> characters = new List<Character> {
             new Character(),
             new Character {Name = "Sam"}
         };
 
         [HttpGet]
+        [Route("GetAll")]
         public ActionResult<List<Character>> Get() {
-            return Ok(knights);
+            return Ok(characters);
+        }
+
+        [HttpGet]
+        public ActionResult<Character> GetSingle() {
+            return Ok(characters[0]);
         }
     }
 }
