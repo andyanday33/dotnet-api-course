8754c401d967e17815746cb363f5b12802695d6a
berke <bab26@st-andrews.ac.uk>
2022-09-14 13:17:17 +0300

    feat: post new character

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -25,5 +25,11 @@ namespace dotnet_api.Controllers
         public ActionResult<Character> GetSingle(int id) {
             return Ok(characters.FirstOrDefault(c => c.Id == id));
         }
+
+        [HttpPost]
+        public ActionResult<List<Character>> AddCharacter(Character newCharacter) {
+            characters.Add(newCharacter);
+            return Ok(characters);
+        }
     }
 }
