c148c425c3e85a2b42b13cae4dd393676c1841aa
berke <bab26@st-andrews.ac.uk>
2022-09-17 00:49:30 +0300

    fix: post character id value

--- Services/CharacterSevice/CharacterService.cs
+++ Services/CharacterSevice/CharacterService.cs
@@ -24,7 +24,9 @@ namespace dotnet_api.Services.CharacterSevice
         public async Task<ServiceResponse<List<GetCharacterDto>>> AddCharacter(AddCharacterDto newCharacter)
         {
             var serviceResponse = new ServiceResponse<List<GetCharacterDto>>();
-            characters.Add(_mapper.Map<Character>(newCharacter));
+            Character character = _mapper.Map<Character>(newCharacter);
+            character.Id = characters.Max(c => c.Id) + 1;
+            characters.Add(character);
             serviceResponse.Data = characters.Select(c => _mapper.Map<GetCharacterDto>(c)).ToList();
             return serviceResponse;
         }
